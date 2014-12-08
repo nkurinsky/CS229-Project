b3a21640365a16e404f291143fcc2870668bf31e
Noah Kurinsky <nkurinsky91@gmail.com>
2014-12-07 17:58:21 -0800

    adding SVM functions

--- src/SVM/svm-linear.py
+++ src/SVM/svm-linear.py
@@ -9,14 +9,11 @@ truth = np.load("../truth.npy")
 
 print(len(data))
 
-gamma_range = 10.0 ** np.arange(-5, 5)
-param_grid = dict(gamma=gamma_range)
-cv = StratifiedKFold(y=truth, n_folds=3)
-clf = GridSearchCV(SVC(class_weight="auto",kernel="rbf",verbose=True,cache_size=1000), param_grid=param_grid, cv=cv, verbose=10)
+clf = SVC(class_weight="auto",kernel="linear",verbose=True,cache_size=1000)
 clf.fit(data, truth)
 
 print(clf.score(data,truth))
 
-output=open("svm-gauss.pkl",'wb')
+output=open("svm-linear.pkl",'wb')
 pickle.dump(clf,output)
 output.close()
--- src/SVM/svm-logistic.py
+++ src/SVM/svm-logistic.py
@@ -10,13 +10,14 @@ truth = np.load("../truth.npy")
 print(len(data))
 
 gamma_range = 10.0 ** np.arange(-5, 5)
-param_grid = dict(gamma=gamma_range)
+r_range= np.arange(-2.0,2.0)
+param_grid = dict(gamma=gamma_range,coef0=r_range)
 cv = StratifiedKFold(y=truth, n_folds=3)
-clf = GridSearchCV(SVC(class_weight="auto",kernel="rbf",verbose=True,cache_size=1000), param_grid=param_grid, cv=cv, verbose=10)
+clf = GridSearchCV(SVC(class_weight="auto",kernel="sigmoid",verbose=True,cache_size=1000), param_grid=param_grid, cv=cv, verbose=10)
 clf.fit(data, truth)
 
 print(clf.score(data,truth))
 
-output=open("svm-gauss.pkl",'wb')
+output=open("svm-sigmoid.pkl",'wb')
 pickle.dump(clf,output)
 output.close()
